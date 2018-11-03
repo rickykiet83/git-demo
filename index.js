2dc831461c88d953134bbc3a676bb78bb6f6bcfd
Kiet Pham <phamquanganhkiet@gmail.com>
2018-11-03 11:21:25 +1100

    feature changed

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log('hello git');
-console.log('new featured is done - master changed and conflicted with featured branch');
+console.log('new featured is done - master changed dsf342 jh23l4h 2l3kj4h32 and conflicted with featured branch');
