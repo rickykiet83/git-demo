6403c0705e9ce706065bf548ac83c537ca5a0646
Kiet Pham <phamquanganhkiet@gmail.com>
2018-11-03 11:35:37 +1100

    merge code from feature

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log('hello git');
-console.log('new featured is done - master changed sfj ;lkj ;kj4 lkj24lk23 and conflicted with featured branch');
+console.log('new featured is done');
+console.log('new featured 2 is done');
+console.log('new featured 3 is done');
